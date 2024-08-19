77dd0ace773b25a6843f6aa8348fb03d041a6e2d
layerist <purotigm@proton.me>
2024-08-19 16:15:47 +0300

    Update eth_price_tracker.py

--- eth_price_tracker.py
+++ eth_price_tracker.py
@@ -5,7 +5,7 @@ import logging
 import sys
 from contextlib import contextmanager
 
-# Настройка логгирования
+# Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Replace 'your_api_key' with your actual CoinMarketCap API key
